b72aac8b054c904dfdefd347df5bb57312230d45
Alexander Timonenkov <alex@talkatoo.com>
2022-06-24 17:28:13 +0300

    First

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
 /**
@@ -17,9 +18,18 @@ Feel free to drop gorilla.mux if you want and use any other solution available.
 main function reads host/port from env just for an example, flavor it following your taste
 */
 
+const (
+	getNameParameterKey = "param"
+)
+
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
-	router := mux.NewRouter()
+	router := httprouter.New()
+
+	router.GET("/name/:"+getNameParameterKey, nameHandler)
+	router.GET("/bad", badHandler)
+	router.POST("/data", dataHandler)
+	router.POST("/headers", headersHandler)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
@@ -35,4 +45,39 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+func nameHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello, %s!", ps.ByName(getNameParameterKey))
+}
+
+func badHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "I got message:\n"+string(data))
+}
+
+func headersHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	a, err := strconv.Atoi(r.Header.Get("a"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	b, err := strconv.Atoi(r.Header.Get("b"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("a+b", strconv.Itoa(a+b))
 }
